0b8559abe0658f88f17db9b50f7624958814d600
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-17 16:03:18 +1100

    🎱 Updated 8-ball command

--- source/commands/8-ball.js
+++ source/commands/8-ball.js
@@ -1,5 +1,20 @@
 const discord = require ("discord.js");
+const jsonfile = require('jsonfile')
+const file = './cfg/plugins.json'
+const chalk = require("chalk")
+const fetch = require("node-fetch");
 
 exports.run = (client, message, args) =>{
-
+    if(jsonfile.readFileSync(file).ball == "true"){
+        let uri = "https://8ball.delegator.com/magic/JSON/" + args;
+    fetch(uri)
+        .then(response => response.json())
+        .then(json => {
+        message.channel.send(json.magic.answer)
+    });
+    }
+    else
+    {
+        console.log(chalk.red("Plugin 8-Ball not enabled. "))
+    }
 }
