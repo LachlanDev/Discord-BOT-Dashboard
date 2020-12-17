ce9d5d3673bab941046982a4586ace78d82f89b3
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-17 16:36:11 +1100

    🐶 Updated dog command

--- source/commands/dog.js
+++ source/commands/dog.js
@@ -1,5 +1,20 @@
 const discord = require ("discord.js");
+const jsonfile = require('jsonfile')
+const file = './cfg/plugins.json'
+const chalk = require("chalk")
+const fetch = require("node-fetch");
 
 exports.run = (client, message, args) =>{
+    if(jsonfile.readFileSync(file).dog == "true"){
+        let uri = "https://dog.ceo/api/breeds/image/random";
+        fetch(uri)
+            .then(response => response.json())
+            .then(json => {
+            message.channel.send(json.message)
+        });
+    }else
+    {
+        console.log(chalk.red("Plugin Dog not enabled. "))
 
+    }
 }
