ce228a9d94b02830ffe277de5e468aa637f93740
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-17 16:41:07 +1100

    🐱 Updated cat command

--- source/commands/cat.js
+++ source/commands/cat.js
@@ -1,5 +1,20 @@
 const discord = require ("discord.js");
+const jsonfile = require('jsonfile')
+const file = './cfg/plugins.json'
+const chalk = require("chalk")
+const fetch = require("node-fetch");
 
 exports.run = (client, message, args) =>{
+    if(jsonfile.readFileSync(file).cat == "true"){
+        let uri = "https://aws.random.cat/meow";
+        fetch(uri)
+            .then(response => response.json())
+            .then(json => {
+            message.channel.send(json.file)
+        });
+    }else
+    {
+        console.log(chalk.red("Plugin Cat not enabled. "))
 
+    }
 }
