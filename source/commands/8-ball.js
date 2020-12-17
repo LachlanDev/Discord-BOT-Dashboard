8072d77418642dbeeb2218a27300243c4fec079d
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-17 16:33:44 +1100

    🎱 Updated 8-ball command

--- source/commands/8-ball.js
+++ source/commands/8-ball.js
@@ -6,12 +6,17 @@ const fetch = require("node-fetch");
 
 exports.run = (client, message, args) =>{
     if(jsonfile.readFileSync(file).ball == "true"){
+    if(args == ""){
+        message.channel.send("Please ask a question.")
+    }else
+    {
         let uri = "https://8ball.delegator.com/magic/JSON/" + args;
-    fetch(uri)
-        .then(response => response.json())
-        .then(json => {
-        message.channel.send(json.magic.answer)
-    });
+        fetch(uri)
+            .then(response => response.json())
+            .then(json => {
+            message.channel.send(json.magic.answer)
+        });
+    }
     }
     else
     {
