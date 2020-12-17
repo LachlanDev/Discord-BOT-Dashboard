d5fea67eca01c2035fb90cd798c1d5ffcde23490
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-17 17:04:12 +1100

    🎲 Updated roll command

--- source/commands/roll.js
+++ source/commands/roll.js
@@ -1,5 +1,14 @@
 const discord = require ("discord.js");
+const jsonfile = require('jsonfile')
+const file = './cfg/plugins.json'
+const chalk = require("chalk")
+const throwdice = () => ~~(Math.random() * 6) + 1;
 
 exports.run = (client, message, args) =>{
-
+    if(jsonfile.readFileSync(file).roll == "true"){
+        message.channel.send("The Number is "+ throwdice())
+    }else
+    {
+        console.log(chalk.red("Plugin Roll not enabled. "))
+    }
 }
