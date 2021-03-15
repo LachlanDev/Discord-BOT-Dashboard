c90dd6eacc0b3679db21ed52f2a82cc541425141
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-03-16 07:54:04 +1100

    Updated footer

--- source/commands/server-info.js
+++ source/commands/server-info.js
@@ -15,7 +15,7 @@ exports.run = (client, message, args) =>{
         .addField("Member Count", `${message.guild.memberCount}`)
         .addField("Server Region", `${message.guild.region}`)
         .addField("Creation Date", dateformat(`${message.guild.createdAt}`, 'dddd, mmmm dS, yyyy, h:MM TT'))
-        .setFooter("Made using Discord BOT Dashboard v1.1 by PapaSnags#1555", "https://avatars1.githubusercontent.com/u/58458169?s=460&u=79564adeae9287fecf24814f64ed89cff91ca358&v=4")
+        .setFooter("Made using Discord BOT Dashboard v1.1 by LachlanDev#8014", "https://avatars1.githubusercontent.com/u/58458169?s=460&u=79564adeae9287fecf24814f64ed89cff91ca358&v=4")
         message.channel.send({embed: info })
     }
     else
